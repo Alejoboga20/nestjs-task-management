9972c3d08df4253615150dd9596bd880be9358a7
Alejo Boga <alejoboga20@gmail.com>
2021-06-28 09:35:54 -0300

    Adding dto in update controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task.-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -37,7 +38,11 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  updateTaskStatus(@Body() status: TaskStatus, @Param('id') id: string) {
+  updateTaskStatus(
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+    @Param('id') id: string,
+  ) {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 
