6f4796f8c70068ba80327b8d789a63a3c67149d8
Alejo Boga <alejoboga20@gmail.com>
2021-06-30 11:54:23 -0300

    Implementing Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,10 +13,16 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task.-status.dto';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.getTasksById(id);
+  }
   /* 
   @Get()
   getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
