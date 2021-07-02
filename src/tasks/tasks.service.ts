9c4cb0b054c4788dc0053157a19d86f173342c26
Alejo Boga <alejoboga20@gmail.com>
2021-07-02 09:49:12 -0300

    Consuming query in service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,6 +13,10 @@ export class TasksService {
     private tasksRepository: TasksRepository,
   ) {}
 
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
+
   async getTasksById(id: string): Promise<Task> {
     const foundTask = await this.tasksRepository.findOne(id);
 
@@ -42,32 +46,4 @@ export class TasksService {
 
     return task;
   }
-
-  /* getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter((task) => {
-        if (task.title.includes(search) || task.description.includes(search)) {
-          return true;
-        }
-        return false;
-      });
-    }
-
-    return tasks;
-  }
-
-  
-
-   */
 }
