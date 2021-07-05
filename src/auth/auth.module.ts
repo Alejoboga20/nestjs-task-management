a6311cef7db4a9d0596054097882632c04948fe1
Alejo Boga <alejoboga20@gmail.com>
2021-07-05 15:34:52 -0300

    Injecting repository in service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserRepository } from './user.repository';
+import { UsersRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
